7be4fe7f9318b8d4088fae2b6092e99f161616e8
andersoncerberos <anderson_cerberos@hotmail.com>
2025-03-26 21:44:41 -0300

    usando componentes e map no react native

--- 05-fundamentos-react-nativeII/App.js
+++ 05-fundamentos-react-nativeII/App.js
@@ -2,16 +2,47 @@ import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import PrimeiroComponente from './componentes/PrimeiroComponente.jsx';
 import JavaScript from './componentes/JavaScript.jsx';
+import Perfil from './componentes/Perfil.jsx';
+import ListaComponente from './componentes/ListaComponente.jsx';
 export default function App() {
   return (
-   
-      <View style={styles.container}>
-        <StatusBar style="auto" />
 
-     
-        <JavaScript />
+    <View style={styles.container}>
+      <StatusBar style="auto" />
 
-      </View>
+      <ListaComponente/>
+
+      {/* <Perfil
+
+        nome="anderson"
+        idade={15}
+        telefone="123456"
+        email="anderson@com"
+
+      />
+
+      <Perfil
+
+        nome="eliane"
+        idade={45}
+        telefone="12345678"
+        email="eliane@com"
+
+      />
+
+      <Perfil
+
+        nome="taynara"
+        idade={26}
+        telefone="123456789"
+        email="tay@com"
+
+      /> */}
+
+
+
+
+    </View>
 
   );
 }
---
+++ 05-fundamentos-react-nativeII/componentes/ListaComponente.jsx
@@ -0,0 +1,52 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function ListaComponente() {
+
+    const listacarros = ["gol", "celta", "palio", "uno", "fiat", "punto", "corola"]
+
+    return (
+        <View style={styles.container}>
+
+            {listacarros.map(
+                (carro) => <Text>{carro}</Text>
+            )}
+
+            {listacarros.map(
+                carro => <Text>{carro}</Text>
+            )
+
+            }
+
+            {listacarros.map(
+                (carro) => {
+                    return (
+                        <View style={styles.containervermelhor}>
+                            <Text>{carro}</Text>
+                        </View>
+                    )
+                }
+            )}
+
+        </View>
+    )
+
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'yellow',
+        borderWidth: 5,
+        padding: 10
+    },
+    texto: {
+        fontsize: 15,
+        fontweight: 700
+    },
+    containervermelhor: {
+        backgroundColor: 'red',
+        borderWidth: 5,
+        padding: 10
+    }
+})
